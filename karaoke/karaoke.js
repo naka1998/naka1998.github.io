6a99bf5d3d223ef34901816bc1660e273fad927b
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-07-08 10:17:33 +0900

    karaoke tochuu

--- karaoke/karaoke.js
+++ karaoke/karaoke.js
@@ -2,24 +2,31 @@ const onkai = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 console.log(onkai);
 const target = document.getElementById("button1");
 //  文字列中の"C"を一斉に置換→"C#"を一斉に置換→…てのはできなかった
+let outputFunmen = "";
 target.addEventListener("click", () => {
   let inputFumen = document.getElementById("fumen").value;
   let inputUpdown = document.getElementById("updown").valueAsNumber;
-  let outputFunmen;
-  for (i = 0; i <= onkai.length; i++) {
-    let targetchar = inputFumen.charAt(i);
-    for (j = 0; onkai[j] != targetchar && j <= onkai.length; j++) {
+  for (i = 0; i <= inputFumen.length; i++) {
+    let targetChar = inputFumen.charAt(i);
+    let j = 0;
+    while (onkai[j] != targetChar && j <= onkai.length) {
+      j += 1;
     }
+    console.log(j);
     //  検索して、見つかった OR j=12で抜ける
-    if (j = 12) {
-
+    let insChar;
+    if (j >= 12) {
+      //  j=12 <=> targetChar = "　" →変換しない
+      insChar = " ";
     } else {
-
+      //  置き換え先を指定
+      let k = inputUpdown + j;
+      insChar = onkai[k];
     }
-    outputFunmen.charAt(-1) = ;// 一番後ろに文字追加
+    console.log(insChar);
+    //  一番後ろに文字追加
+    outputFunmen += insChar;
   }
-  console.log(inputFumen);
-  document.getElementById("output").value = inputFumen;
+  console.log(outputFunmen);
+  document.getElementById("output").value = outputFunmen;
 }, false);
-
-
